fca9441da64731256223e42302f5f4a7a48b5d73
Andrey <dovban.andreyy@rambler.ru>
2023-02-09 13:45:19 +0300

    работа с файлофой системой

---
+++ services/storage.service.js
@@ -0,0 +1,54 @@
+import { homedir } from 'os';
+import { join } from 'path';
+import { promises } from 'fs';
+// import {
+//     join,
+//     basename,
+//     dirname,
+//     extname,
+//     relative,
+//     isAbsolute,
+//     resolve,
+//     sep,
+// } from 'path';
+// const filePath = join(homedir(), 'weather-data.json');
+
+// export default function saveKeyValue(key, value) {
+// console.log(extname(filePath));
+// console.log(relative(filePath, dirname(filePath)));
+// console.log(isAbsolute(filePath));
+// console.log(resolve('..'));
+// console.log(sep);
+// }
+const filePath = join(homedir(), 'weather-data.json');
+
+async function isExist(path) {
+    try {
+        await promises.stat(path);
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
+
+async function saveKeyValue(key, value) {
+    let data = {};
+    if (await isExist(filePath)) {
+        const file = await promises.readFile(filePath);
+        const data = JSON.parse(file);
+    }
+    data[key] = value;
+    await promises.writeFile(filePath, JSON.stringify(data));
+}
+
+async function getKeyValue(key) {
+    if (await isExist(filePath)) {
+        const file = await promises.readFile(filePath);
+        const data = JSON.parse(file);
+        return data[key];
+    } else {
+        return undefined;
+    }
+}
+
+export { getKeyValue, saveKeyValue };
--- weather.js
+++ weather.js
@@ -1,7 +1,16 @@
 #!usr/bin/emv node
 import getArgs from './helpers/args.js';
-import { printHelp } from './services/log.service.js';
-import saveKeyValue from './storage.service.js';
+import { printHelp, printError, printSuccess } from './services/log.service.js';
+import { saveKeyValue } from './services/storage.service.js';
+
+async function saveToken(token) {
+    try {
+        await saveKeyValue('token', token);
+        printSuccess('Токен сохранён');
+    } catch (error) {
+        printError(error.message);
+    }
+}
 
 function initCLI() {
     const args = getArgs(process.argv);
@@ -11,7 +20,7 @@ function initCLI() {
     if (args.s) {
     }
     if (args.t) {
-        saveKeyValue('token', args.t);
+        return saveToken(args.t);
     }
 }
 
